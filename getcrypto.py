df64a677920da832811e76486ca91541ad0ff8df
Roman Müller <romanmueller@gmx.ch>
2021-02-11 22:01:43 +0100

    added ticker parameter and update_price method

--- getcrypto.py
+++ getcrypto.py
@@ -15,36 +15,42 @@ def get_change(current, previous):
     except ZeroDivisionError:
         return 0
 
+def update_price(ticker):
+    cc.cryptocompare._set_api_key_parameter(secrets.API_KEY_CRYPTOCOMPARE)
+    
+    prices = cc.cryptocompare.get_historical_price_minute(ticker, currency='USD', limit=60)
+
+    price0 = prices[0]
+    priceN = prices[60]
+
+    change = get_change(priceN['close'], price0['close'])
+
+    currentPrice = '${}'.format(round(priceN['close']))
+    print('current price: ' + currentPrice)
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(priceN['time'])))
+
+    print('old price: ' + '${}'.format(price0['close']))
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(price0['time'])))
+
+    print(change * 100)
+    percentage = '{:.1%}'.format(change)
+    print(percentage)
+
+    scrollphat.set_brightness(55)
+    scrollphat.write_string('BTC ' + currentPrice + ' | ' +percentage , 11)
+
+
 print("""
 Press Ctrl+C to exit!
 """)
 
-cc.cryptocompare._set_api_key_parameter(secrets.API_KEY_CRYPTOCOMPARE)
+if len(sys.argv) != 2:
+    print("""
+Usage: {} "ticker"
+""".format(sys.argv[0]))
+    sys.exit(0)
 
-cc.cryptocompare.get_price('BTC', currency='USD')
-
-prices = cc.cryptocompare.get_historical_price_minute('BTC', currency='USD', limit=60)
-
-price0 = prices[0]
-priceN = prices[60]
-
-change = get_change(priceN['close'], price0['close'])
-
-currentPrice = '${}'.format(round(priceN['close']))
-print('current price: ' + currentPrice)
-print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(priceN['time'])))
-
-print('old price: ' + '${}'.format(price0['close']))
-print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(price0['time'])))
-
-print(change * 100)
-percentage = '{:.1%}'.format(change)
-print(percentage)
-
-scrollphat.set_brightness(55)
-scrollphat.write_string('BTC ' + currentPrice + ' | ' +percentage , 11)
-
-servo.moveTo(0)
+ticker = sys.argv[1]
 
 timestamp = int(time.time())
 
@@ -56,8 +62,5 @@ while True:
         scrollphat.clear()
         sys.exit(-1)
     if int(time.time()-timestamp) > 10*1000:
-
-
         scrollphat.clear()
-        servo.moveTo(15)
-
+        update_price(ticker)
